c3c184a37210cfac2bcf24074f4dbc347f0f0a48
giacomo <giacomo.casali@stud.unifi.it>
2023-05-30 16:48:28 +0200

    Added the possibility to view user details.

--- RealEstateWebsitePPM/accounts/views.py
+++ RealEstateWebsitePPM/accounts/views.py
@@ -1,12 +1,19 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, TemplateView
 
 # Create your views here.
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class ProfileView(DetailView):
+    model = CustomUser
+    template_name = "profile_details.html"
+
