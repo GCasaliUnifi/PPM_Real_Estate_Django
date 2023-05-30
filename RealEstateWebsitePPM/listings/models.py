21ea692b83f92a4dd9e26adf7b1bf43e90a6343d
giacomo <giacomo.casali@stud.unifi.it>
2023-05-30 12:37:25 +0200

    Fixed bugs with the way an image was saved. Also made a special function that deletes the image and folder of a listing when that listing is being deleted.

--- RealEstateWebsitePPM/listings/models.py
+++ RealEstateWebsitePPM/listings/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
-import uuid, os
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+import uuid
+import os
 
 # Create your models here.
 
@@ -18,10 +21,13 @@ class Category(models.Model):
 
 
 def listing_image_directory_path(instance, filename):
-    # Generate a unique filename for the image (uuid)
+    # Generate a UUID for the listing
     listing_uuid = uuid.uuid4().hex
-    # Upload to a directory named after the listing uuid
-    return os.path.join('listings', listing_uuid, filename)
+    # Create a unique folder name using the UUID and the listing's ID
+    folder_name = f"listing_{instance.owner}_{listing_uuid}"
+    # Upload image to a directory named after the unique folder name
+    return os.path.join('listings', folder_name, filename)
+
 
 
 class Listing(models.Model):
@@ -33,7 +39,7 @@ class Listing(models.Model):
     square_feet = models.PositiveIntegerField()
     # TODO add location field
     listing_location = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="listing_image_directory_path", blank=True, null=True)
+    image = models.ImageField(upload_to=listing_image_directory_path, blank=True, null=True)
     is_published = models.BooleanField()
     is_featured = models.BooleanField(default=False)
     list_date = models.DateTimeField(auto_now_add=True)
@@ -48,3 +54,19 @@ class Listing(models.Model):
 
     def get_absolute_url(self):
         return reverse("property_detail", kwargs={"pk": self.pk})
+
+
+@receiver(pre_delete, sender=Listing)
+def delete_listing(sender, instance, **kwargs):
+    # Delete associated image
+    if instance.image:
+        image_path = instance.image.path
+        instance.image.delete()
+        if os.path.exists(image_path):
+            os.remove(image_path)
+
+        directory_path = os.path.dirname(image_path)
+        try:
+            os.removedirs(directory_path)
+        except OSError:
+            pass
