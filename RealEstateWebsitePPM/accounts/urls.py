cb5aac8e19a0557712b2c718645191e4120a5ec5
giacomo <giacomo.casali@stud.unifi.it>
2023-06-04 23:08:03 +0200

    Made it possible for users to edit their profile info

--- RealEstateWebsitePPM/accounts/urls.py
+++ RealEstateWebsitePPM/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, ProfileView
+from .views import SignUpView, ProfileView, EditProfileView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/<int:pk>/edit", EditProfileView.as_view(), name="profile_edit"),
     path("profile/<int:pk>", ProfileView.as_view(), name="profile_details"),
 ]
--- RealEstateWebsitePPM/accounts/views.py
+++ RealEstateWebsitePPM/accounts/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, TemplateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 # Create your views here.
 
@@ -7,6 +7,7 @@ from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 
+# TODO ricorda di aggiungere il LoginRequiredMixin a tutte le classi
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
@@ -17,3 +18,10 @@ class ProfileView(DetailView):
     model = CustomUser
     template_name = "profile_details.html"
 
+
+class EditProfileView(UpdateView):
+    model = CustomUser
+    # fields = ['first_name', 'last_name', 'phone_number', 'date_of_birth', 'address']
+    fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth', 'address']
+    template_name = "registration/profile_edit.html"
+    success_url = reverse_lazy("profile_details")
