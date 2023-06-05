6d9e52c00a8d0faa15f55ad179a085d7ca4c96a1
giacomo <giacomo.casali@stud.unifi.it>
2023-06-05 10:25:45 +0200

    Now the edit form displays its values before the edit correctly.

--- RealEstateWebsitePPM/accounts/models.py
+++ RealEstateWebsitePPM/accounts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
 
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(validators=[EmailValidator(message='Please enter a valid email address.')], unique=True, blank=False, null=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=100, null=True, blank=True)
